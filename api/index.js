859d4ed8ad814da63d0ec9935f0a569bbfb181ac
Kanishka Panwar <kanishkap2002@gmail.com>
2022-06-29 23:46:31 +0530

    error handling

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import authRoute from "./routes/auth.js";
@@ -21,10 +21,24 @@ mongoose.connection.on("disconnected", () => {
   console.log("mongodb disconnected!");
 });
 
+//middlewares
+app.use(express.json());
+
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500
+  const errorMessage = err.message || "something went wrong"
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
 
 app.listen(8800, () => {
   connect();
