5c719204af7b0308fb69cc40b9bebb9cc176ca70
Kanishka Panwar <kanishkap2002@gmail.com>
2022-06-29 15:31:15 +0530

    fixed header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -84,7 +84,7 @@ const Header = ({ type }) => {
             </h1>
             <p className="headerDesc">
               Get rewarded for your travels - unlock instant savings of 10% or
-              more with a free Quick Fix Stay account
+              more with a free Quick Fix Stays account
             </p>
             <button className="headerBtn">Sign in / Register</button>
             <div className="headerSearch">
