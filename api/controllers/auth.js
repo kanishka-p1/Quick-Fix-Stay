20910e68d54ccf8327388d6aa75a996599bdb2ba
Kanishka Panwar <kanishkap2002@gmail.com>
2022-06-30 13:46:49 +0530

    error handling

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,7 @@
 import User from "../models/User.js";
+import { createError } from "../utils/error.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res, next) => {
   try {
@@ -29,10 +31,20 @@ export const login = async (req, res, next) => {
       user.password
     );
     if (!isPasswordCorrect)
-      return next(404, "Password incorrect or incorrect username");
+      return next(createError(404, "Password incorrect or incorrect username"));
+
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
 
     const { password, isAdmin, ...otherDetails } = user._doc;
-    res.status(200).json({ ...otherDetails });
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ details: { ...otherDetails }, isAdmin });
   } catch (err) {
     next(err);
   }
